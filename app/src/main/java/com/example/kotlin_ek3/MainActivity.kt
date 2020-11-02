03975b1e54e666d27e696b61d1a6ad08b4bbfed4
ltrebicki-tgm <ltrebicki@student.tgm.ac.at>
2020-11-02 17:14:25 +0100

    Simple onClick Methode

--- app/src/main/java/com/example/kotlin_ek3/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ek3/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.kotlin_ek3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun addOnClick(view: View) {
+        var textview = findViewById<TextView>(R.id.textfield)
+        var text = textview.text.toString()
+        var nummer = text.toInt()+1
+        textview.text = nummer.toString()
     }
 }
