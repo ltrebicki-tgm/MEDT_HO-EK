62f471f74242d8819ded668e30156a321239080b
ltrebicki-tgm <ltrebicki@student.tgm.ac.at>
2020-11-13 14:55:10 +0100

    SplashScreen

---
+++ app/src/main/java/com/example/kotlin_ek3/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin_ek3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        splashLogo.alpha = 0f
+        splashLogo.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
