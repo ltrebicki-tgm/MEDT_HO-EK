a7e54b6cae968f6fb806a67df1226ee9ec350a46
ltrebicki-tgm <ltrebicki@student.tgm.ac.at>
2020-11-13 12:43:00 +0100

    Notifications

--- app/src/main/java/com/example/kotlin_ek3/FalschFragment.kt
+++ app/src/main/java/com/example/kotlin_ek3/FalschFragment.kt
@@ -1,12 +1,20 @@
 package com.example.kotlin_ek3
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavDeepLinkBuilder
 import androidx.navigation.findNavController
 import com.example.kotlin_ek3.databinding.FragmentFalschBinding
 
@@ -24,7 +32,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SchlechtFragment : Fragment() {
-
+    val CHANNEL_ID = "channelID"
+    val CHANNEL_NAME = "channelName"
+    val NOTIFICAITON_ID = 0
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -36,6 +46,39 @@ class SchlechtFragment : Fragment() {
         binding.buttonNeuerVersuch.setOnClickListener{view: View->
             view.findNavController()
                 .navigate(R.id.action_falschFragment_to_mainFragment)}
+
+        val pendingIntent = NavDeepLinkBuilder(context!!)
+            .setGraph(R.navigation.navigation)
+            .setDestination(R.id.mainFragment)
+            .createPendingIntent()
+
+        createNotificationChannel()
+        val notification = NotificationCompat.Builder(context!!, CHANNEL_ID)
+            .setContentTitle("Leider falsch!")
+            .setContentText("Hier kannst du es nochmal versuchen!")
+            .setSmallIcon(R.drawable.ic_star_border_24px)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)    // um zu canceln wenn User anklickt
+            .build()
+
+
+        val notificationManager = NotificationManagerCompat.from(context!!)
+        notificationManager.notify(NOTIFICAITON_ID, notification)
+
         return binding.root
+    }
+
+    fun createNotificationChannel() {
+        // Oreo oder neuer?
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.RED
+                enableLights(true)
+            }
+            val manager = this.activity?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
     }
 }
--- app/src/main/java/com/example/kotlin_ek3/RichtigFragment.kt
+++ app/src/main/java/com/example/kotlin_ek3/RichtigFragment.kt
@@ -1,12 +1,22 @@
 package com.example.kotlin_ek3
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavDeepLinkBuilder
 import androidx.navigation.findNavController
 import com.example.kotlin_ek3.databinding.FragmentRichtigBinding
 
@@ -24,7 +34,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class GutFragment : Fragment() {
-
+    val CHANNEL_ID = "channelID"
+    val CHANNEL_NAME = "channelName"
+    val NOTIFICAITON_ID = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,6 +47,39 @@ class GutFragment : Fragment() {
         binding.buttonNeustart.setOnClickListener{view: View->
             view.findNavController()
                 .navigate(R.id.action_richtigFragment_to_mainFragment)}
+
+        val pendingIntent = NavDeepLinkBuilder(context!!)
+            .setGraph(R.navigation.navigation)
+            .setDestination(R.id.mainFragment)
+            .createPendingIntent()
+
+        createNotificationChannel()
+        val notification = NotificationCompat.Builder(context!!, CHANNEL_ID)
+            .setContentTitle("Glückwunsch!")
+            .setContentText("Möchtest du ein neues Spiel starten?")
+            .setSmallIcon(R.drawable.ic_star_24px)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)    // um zu canceln wenn User anklickt
+            .build()
+
+
+        val notificationManager = NotificationManagerCompat.from(context!!)
+        notificationManager.notify(NOTIFICAITON_ID, notification)
+
         return binding.root
+    }
+
+    fun createNotificationChannel() {
+        // Oreo oder neuer?
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = this.activity?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
     }
 }
